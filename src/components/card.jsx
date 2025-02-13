d77461431701ad8cce4339fcfe8c300a5fc233e6
Sandblaze05 <tejaschauhan0512@gmail.com>
2025-02-13 19:14:46 +0530

    Add AnimeDetails page and integrate react-router-dom for navigation

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Card = ({ title, image }) => {
+const Card = ({ title, image, onClick }) => {
   return (
     <div className="relative w-44 h-68 mb-4 rounded-xl overflow-hidden backdrop-blur-xl 
                     transform transition duration-300 hover:scale-105 cursor-pointer group
                     shadow-lg shadow-[#ff3d7f]/30 hover:shadow-[#ff3d7f]/50
-                    border border-[#ff3d7f]/20 bg-gradient-to-b from-[#1a0e2b]/60 to-[#290a4a]/60">
+                    border border-[#ff3d7f]/20 bg-gradient-to-b from-[#1a0e2b]/60 to-[#290a4a]/60"
+                    onClick={onClick}>
       {/* Anime Poster */}
       <img className="w-full h-full object-cover" src={image} alt={title} />
 
